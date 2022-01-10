f270c2be161061b490a0db276b9d0825399fbc78
Bino Faata <maximleluk@gmail.com>
2022-01-11 01:44:36 +0200

    Task 4 done

--- task_4.php
+++ task_4.php
@@ -30,42 +30,65 @@
                             <button class="btn btn-panel waves-effect waves-themed" data-action="panel-fullscreen" data-toggle="tooltip" data-offset="0,10" data-original-title="Fullscreen"></button>
                         </div>
                     </div>
+                    <?php
+$items = [
+    [
+        "title" => "My Tasks",
+        "value" => "130 /500",
+        "bg" => "bg-fusion-400",
+        "width" => "65%",
+        "valuenow" => "65",
+        "valuemin" => "0",
+        "valuemax" => "100",
+    ],
+    [
+        "title" => "Transfered",
+        "value" => "440 Tb",
+        "bg" => "bg-success-500",
+        "width" => "34%",
+        "valuenow" => "34",
+        "valuemin" => "0",
+        "valuemax" => "100",
+    ],
+    [
+        "title" => "Bugs Squashed",
+        "value" => "77%",
+        "bg" => "bg-info-400",
+        "width" => "77%",
+        "valuenow" => "77",
+        "valuemin" => "0",
+        "valuemax" => "100",
+    ],
+    [
+        "title" => "User Testing",
+        "value" => "7 days",
+        "bg" => "bg-primary-300",
+        "width" => "84%",
+        "valuenow" => "84",
+        "valuemin" => "0",
+        "valuemax" => "100",
+    ],
+]
+?>
+
                     <div class="panel-container show">
                         <div class="panel-content">
-                            <div class="d-flex mt-2">
-                                My Tasks
-                                <span class="d-inline-block ml-auto">130 / 500</span>
-                            </div>
-                            <div class="progress progress-sm mb-3">
-                                <div class="progress-bar bg-fusion-400" role="progressbar" style="width: 65%;" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                            <div class="d-flex">
-                                Transfered
-                                <span class="d-inline-block ml-auto">440 TB</span>
-                            </div>
-                            <div class="progress progress-sm mb-3">
-                                <div class="progress-bar bg-success-500" role="progressbar" style="width: 34%;" aria-valuenow="34" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                            <div class="d-flex">
-                                Bugs Squashed
-                                <span class="d-inline-block ml-auto">77%</span>
-                            </div>
-                            <div class="progress progress-sm mb-3">
-                                <div class="progress-bar bg-info-400" role="progressbar" style="width: 77%;" aria-valuenow="77" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                            <div class="d-flex">
-                                User Testing
-                                <span class="d-inline-block ml-auto">7 days</span>
-                            </div>
-                            <div class="progress progress-sm mb-g">
-                                <div class="progress-bar bg-primary-300" role="progressbar" style="width: 84%;" aria-valuenow="84" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
+<?php foreach ($items as $item): ?>
+    <div class="d-flex mt-2">
+        <?php echo $item["title"] ?>
+        <span class="d-inline-block ml-auto"><?php echo $item["value"]; ?></span>
+    </div>
+    <div class="progress progress-sm mb-3">
+        <div class="progress-bar <?php echo $item["bg"]; ?>" role="progressbar" style="width: <?php echo $item["width"]; ?>;" aria-valuenow="<?php echo $item["valuenow"]; ?>" aria-valuemin="<?php echo $item["valuemin"]; ?>" aria-valuemax="<?php echo $item["valuemax"]; ?>"></div>
+    </div>
+<?php endforeach;?>
+
                         </div>
                     </div>
                 </div>
             </div>
         </main>
-        
+
 
         <script src="js/vendors.bundle.js"></script>
         <script src="js/app.bundle.js"></script>
